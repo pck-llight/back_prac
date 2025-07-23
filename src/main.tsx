3015a2e97f9da62269df62f230b6eeafdb3d80dc
박찬규 <126973231+pck-llight@users.noreply.github.com>
2025-07-23 20:54:03 +0900

    새로운 마음으로 다시 시작해보겠습니다..

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,5 @@
-
-import { createRoot } from 'react-dom/client'
+import { render } from 'preact'
 import './index.css'
-import Home from './pages/Home'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Post from './pages/Post'
+import { App } from './app.tsx'
 
-const queryClient = new QueryClient()
-
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>,
-)
+render(<App />, document.getElementById('app')!)
